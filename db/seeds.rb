5cba4b7b92ea503353ce28b7f3bcd3f8dc649f9a
Goncalo Cordeiro <glevycordeiro@gmail.com>
2020-02-08 12:07:27 +0000

    app finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts 'Creating 10 fake ingredients...'
+10.times do
+  ingredient = Ingredient.new(
+    name:    Faker::Food.ingredient
+  )
+  if ingredient.save
+    ingredient.save!
+  end
+end
+puts 'Finished!'
+
+puts 'Creating 10 fake cocktails...'
+10.times do
+  cocktail = Cocktail.new(
+    name:    Faker::Food.dish
+  )
+  if cocktail.save
+    cocktail.save!
+  end
+end
+puts 'Finished!'
+
+puts 'Creating 10 fake doses...'
+10.times do
+  dose = Dose.new(
+    description: "#{rand(10..30)} cl",
+    cocktail: Cocktail.all.sample,
+    ingredient: Ingredient.all.sample
+  )
+  if dose.save
+    dose.save!
+  end
+end
+puts 'Finished!'
